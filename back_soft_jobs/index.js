f003a1e455bb063ba4880062d188c852b8ed989b
Nicolás San Martin <nicolas.sm.lor@gmail.com>
2025-02-26 19:14:11 -0300

    implementacion correcta de logs

--- back_soft_jobs/index.js
+++ back_soft_jobs/index.js
@@ -1,18 +1,23 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
-import morgan from "morgan";
+import { fileURLToPath } from "url";
+import path from "path";
+import loggerMiddleware from "./middlewares/logger.js";
 import softJobsRoute from "./routes/softjobs.route.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan("dev"));
+app.use(loggerMiddleware);
 app.use("/soft-jobs", softJobsRoute);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () =>
-  console.log("Servidor levantado con éxito! --> http://localhost:3000/"),
+  console.log("Servidor levantado con éxito! --> http://localhost:" + PORT),
 );
